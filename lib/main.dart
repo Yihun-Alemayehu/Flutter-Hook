e7358580509eef78497650c5ab0477ad111535bd
Yihun Alemayehu <yankure01@gmail.com>
2023-11-30 09:29:31 +0300

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      useMaterial3: true,
-    ),
-    home: const HomePage(),
+    home: HomePage(),
   ));
 }
 
-class HomePage extends StatelessWidget {
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dateTime = useStream(getTime());
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text(dateTime.data ?? 'Home Page'),
         centerTitle: true,
       ),
       body: Container(),
