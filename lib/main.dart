30a35582c43f6c6d0605e5f91c5845fbb408ef4c
Yihun Alemayehu <yankure01@gmail.com>
2023-11-30 20:25:50 +0300

    another commit 30

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,49 @@ void main() {
   ));
 }
 
-Stream<String> getTime() => Stream.periodic(
-      const Duration(seconds: 1),
-      (_) => DateTime.now().toIso8601String(),
-    );
+// Stream<String> getTime() => Stream.periodic(
+//       const Duration(seconds: 1),
+//       (_) => DateTime.now().toIso8601String(),
+//     );
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    //final dateTime = useStream(getTime());
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(
+      () {
+        controller.addListener(() {
+          text.value = controller.text;
+        });
+        return null;
+      },
+      [controller],
+    );
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? 'Home Page'),
+        title: const Text('Home Page'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              )),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
