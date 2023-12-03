aa096f1aef91c623d6a622c5ab69266402319aa1
Yihun Alemayehu <yankure01@gmail.com>
2023-12-03 11:03:20 +0300

    example six commit

---
+++ lib/examples/example_6.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+const url = 'https://avatars.githubusercontent.com/u/101422490?v=4';
+
+class ExampleSix extends HookWidget {
+  const ExampleSix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'H O M E',
+          style: TextStyle(
+            letterSpacing: 3,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:time_ticking/examples/example_2.dart';
 import 'package:time_ticking/examples/example_3.dart';
 import 'package:time_ticking/examples/example_4.dart';
 import 'package:time_ticking/examples/example_5.dart';
+import 'package:time_ticking/examples/example_6.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ExampleFive(),
+      home: ExampleSix(),
     ),
   );
 }
